14f597a7adefb75e94c8b9b0f727df59c1d2ae50
dj4t9n <pandeyjatin47@gmail.com>
2021-10-30 11:38:26 +0530

    class for image and message

--- test.py
+++ test.py
@@ -2,24 +2,59 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Message():
+    def __init__(self, content):
+        self.content = content
 
-image = cv2.imread("test.png")
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    def to_bin(self):
+        if type(self.content) == str:
+            return ''.join(
+            [format(ord(i), "08b") for i in self.content])
 
-pixel_values = image.reshape((-1, 3))
-pixel_values = np.float32(pixel_values)
+        elif type(self.content) == bytes or type(self.content) == np.ndarray:
+            return [format(i, "08b") for i in self.content ]
+        elif type(self.content) == int or type(self.content) == np.uint8:
+            return format(self.content, "08b")
+        else:
+            raise TypeError("Input type not supported")
 
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+class Image():
+    def __init__(self, path):
+        self.path = path
+        self.image = cv2.cvtColor(
+                cv2.imread(self.path), 
+                cv2.COLOR_BGR2RGB)
 
-k = 7
-_, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    def get_pxl_vals(self):
+        return np.float32(self.image.reshape((-1, 3)))
 
-centers = np.uint8(centers)
-labels = labels.flatten()
+    def __repr__(self):
+        return f"image at {self.path}"
 
-segmented_image = centers[labels.flatten()]
+    def cluster(self, k):
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
 
-segmented_image = segmented_image.reshape(image.shape)
+        _, self.labels, (self.centers) = cv2.kmeans(
+                self.get_pxl_vals(), 
+                k, 
+                None, 
+                criteria, 
+                10, 
+                cv2.KMEANS_RANDOM_CENTERS)
 
-cv2.imwrite('out.png', segmented_image)
-print("DONE")
+        self.centers = np.uint32(self.centers)
+        self.labels = self.labels.reshape(1, -1)
+
+
+    def get_indx_for_cluster(self, k):
+        return np.where(self.labels == k)
+
+if __name__ == "__main__":
+    image = Image("test.png")
+    pixel_values = image.get_pxl_vals()
+
+    k = 7
+    image.cluster(k)
+    image.labels
+    image.centers
+    print("DONE")
